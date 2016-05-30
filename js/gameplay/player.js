580f8abee8fd2a0018a22737867ad2e9bc1daaa7
Bari Mourdjen <liaroc13@gmail.com>
2016-05-30 19:56:16 +0200

    try to correct every second the size of the canvas

--- js/gameplay/player.js
+++ js/gameplay/player.js
@@ -8,10 +8,19 @@ var Player = function(g) {
 	console.log(g.canvasHeight);
 	this.containerPlayer.width = g.canvasWidth * 0.9;
 	this.containerPlayer.height = g.canvasHeight * 0.5;
+	this.countChangeSizePlayer = 0;
+	console.log("container player: ",this.containerPlayer);
 	this.containerPlayer.pos = new Vector2(g.canvasWidth * 0.05, 0);
 };
 Player.prototype.update = function(g,currentTime,duration) {
-	this.pos.x = this.containerPlayer.pos.x + (this.containerPlayer.width/duration*currentTime);
+	this.countChangeSizePlayer++;
+	if(this.countChangeSizePlayer % 60 == 0){
+		console.log('CHANGE');
+		this.changeSizeCanvas(g);
+	}
+	// console.log('update: ', this.containerPlayer.pos.x, this.containerPlayer.width,duration,currentTime);
+	// console.log('my pos: ', this.pos);
+	this.pos.x = this.containerPlayer.pos.x + ((this.containerPlayer.width/duration)*currentTime);
 };
 Player.prototype.render = function(g) {
 	g.context.drawImage(this.img,this.pos.x,this.pos.y);
@@ -22,4 +31,14 @@ Player.prototype.click = function(pos,which,currentTime) {
 	&& pos.y >= this.containerPlayer.pos.y && pos.y <= this.containerPlayer.height + this.containerPlayer.pos.y){
 		console.log(currentTime);
 	}
+};
+Player.prototype.verifiModifSizePlayer = function(g){
+
+};
+Player.prototype.changeSizeCanvas = function(g) {
+	var canvas = document.getElementById("emotion-player");
+	var tempCanvas = canvas.getBoundingClientRect();
+	g.canvasWidth = tempCanvas.width;
+	this.containerPlayer.width = g.canvasWidth * 0.9;
+	this.containerPlayer.height = g.canvasHeight * 0.5;
 };
